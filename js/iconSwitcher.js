ca58c8b2b058e58b1399619dbc3b764a41e5bf9f
Payson Blackwell <paysonblackwell@gmail.com>
2019-07-25 16:59:45 -0700

    icon switching for design icon

---
+++ js/iconSwitcher.js
@@ -0,0 +1,8 @@
+function hover(element, name) {
+    element.setAttribute('src', 'images/servicesIcons/'+name+'_OTHER.svg');
+    
+  }
+  
+  function unhover(element, name) {
+    element.setAttribute('src', 'images/servicesIcons/'+name+'.svg');
+  }
