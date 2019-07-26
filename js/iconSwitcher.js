eec470fe379c9d16006f489cc4f5f9e6a1c1bada
Kobe Kapoor <kobekapoor@gmail.com>
2019-07-25 17:06:20 -0700

    background change on hover icon

--- js/iconSwitcher.js
+++ js/iconSwitcher.js
@@ -1,8 +1,9 @@
 function hover(element, name) {
     element.setAttribute('src', 'images/servicesIcons/'+name+'_OTHER.svg');
-    
+    element.style.background = "#E7E7E7"
   }
   
   function unhover(element, name) {
     element.setAttribute('src', 'images/servicesIcons/'+name+'.svg');
+    element.style.background = "white"
   }
