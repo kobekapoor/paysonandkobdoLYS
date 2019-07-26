37f5271d407a4b811613d227a55ee141534ba88c
Payson Blackwell <paysonblackwell@gmail.com>
2019-07-25 17:19:43 -0700

    fading

--- js/iconSwitcher.js
+++ js/iconSwitcher.js
@@ -1,15 +1,19 @@
 function hover(element, name) {
+
+
+    $(element).animate({'opacity': 0}, 1000, function () {
+        $(this).attr('src','images/servicesIcons/'+name+'_OTHER.svg')
+    }).animate({'opacity': 1}, 1000);
+
+
     //element.setAttribute('src', 'images/servicesIcons/'+name+'_OTHER.svg');
-    $(element).fadeOut(300, function(){
-        $(this).attr('src','images/servicesIcons/'+name+'_OTHER.svg').bind('onreadystatechange load', function(){
-           if (this.complete) 
-            $(this).fadeIn(290);
-        });
-    });
-    element.style.background = "#E7E7E7"
+
   }
   
   function unhover(element, name) {
-    element.setAttribute('src', 'images/servicesIcons/'+name+'.svg');
-    element.style.background = "white"
+    $(element).animate({'opacity': 0}, 1000, function () {
+        $(this).attr('src','images/servicesIcons/'+name+'.svg')
+    }).animate({'opacity': 1}, 1000);
   }
+
+  
